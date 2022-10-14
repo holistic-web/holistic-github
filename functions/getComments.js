c958a4b85c2e98080f465ad8f03a89f15293a813
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2022-10-14 15:30:23 -0700

    fix: cloud functions deploy

--- functions/getComments.js
+++ functions/getComments.js
@@ -1,7 +1,6 @@
 const functions = require("firebase-functions");
 
 module.exports = functions.https.onRequest((request, response) => {
-  console.log("request: ", request);
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
