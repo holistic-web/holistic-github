f94a06346f3207e8fc164f388b8b9a2557150b22
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2022-10-14 15:28:06 -0700

    chore: modify function

--- functions/getComments.js
+++ functions/getComments.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 
 module.exports = functions.https.onRequest((request, response) => {
+  console.log("request: ", request);
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
