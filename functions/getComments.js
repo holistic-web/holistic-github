cdad108130695988c92997b118ef933755603b3c
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2022-10-15 14:02:13 -0700

    feat: add octokit to load database from github

--- functions/getComments.js
+++ functions/getComments.js
@@ -1,9 +1,19 @@
 const functions = require("firebase-functions");
-// const Octokit = require("octokit");
+const Octokit = require("octokit");
 
-// const octokit = new Octokit({auth: "personal-access-token123"});
+const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+const octokit = new Octokit({auth: GITHUB_ACCESS_TOKEN});
 
-module.exports = functions.https.onCall((userId) => {
+module.exports = functions.https.onCall(async (userId) => {
+  const {
+    data: {login},
+  } = await octokit.rest.users.getAuthenticated();
+  console.log("Hello, %s", login);
+  console.log("Hello, %s", login);
+
   functions.logger.info("Hello logs!", {structuredData: true});
-  return `Hello from Firebase!, userId=${userId}`;
+  return {
+    login,
+    userId,
+  };
 });
