8d678bd077e9f4e38ef00fd75118bfde8bcbe807
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2022-10-15 15:17:57 -0700

    fix: linting

--- functions/getIssues.js
+++ functions/getIssues.js
@@ -6,11 +6,11 @@ const octokit = new Octokit({auth: GITHUB_TOKEN});
 
 const getIssues = async (userId) => {
   const response = await octokit.rest.search.issuesAndPullRequests({
-    q:`is:issue author:${userId}`,
-    per_page:100
+    q: `is:issue author:${userId}`,
+    per_page: 100,
   });
-  return response.data.items
-}
+  return response.data.items;
+};
 
 module.exports = functions.https.onCall(async (userId) => {
   const issues = await getIssues(userId);
