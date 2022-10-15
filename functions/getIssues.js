b8157b0ac2647d1b82328b41592d8e5723af6187
Jack Dewinter <jackdewinter1@gmail.com>
2022-10-15 23:12:00 +0100

    get some issues for a user

--- functions/getIssues.js
+++ functions/getIssues.js
@@ -4,16 +4,18 @@ const {Octokit} = require("octokit");
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const octokit = new Octokit({auth: GITHUB_TOKEN});
 
+const getIssues = async (userId) => {
+  const response = await octokit.rest.search.issuesAndPullRequests({
+    q:`is:issue author:${userId}`,
+    per_page:100
+  });
+  return response.data.items
+}
+
 module.exports = functions.https.onCall(async (userId) => {
-  const {
-    data: {login},
-  } = await octokit.rest.users.getAuthenticated();
-  console.log("Hello, %s", login);
+  const issues = await getIssues(userId);
 
   functions.logger.info("Hello logs!", {structuredData: true});
-  return {
-    login,
-    userId,
-  };
+  return issues;
 });
 
