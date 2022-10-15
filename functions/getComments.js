0a7de63cd7726e4783acde915e8b973829679929
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2022-10-15 13:41:31 -0700

    chore: install octokit

--- functions/getComments.js
+++ functions/getComments.js
@@ -1,4 +1,7 @@
 const functions = require("firebase-functions");
+// const Octokit = require("octokit");
+
+// const octokit = new Octokit({auth: "personal-access-token123"});
 
 module.exports = functions.https.onCall((userId) => {
   functions.logger.info("Hello logs!", {structuredData: true});
