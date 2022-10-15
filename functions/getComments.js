9c2e604f5740b6af00b95b2b36f3e80b8be68d25
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2022-10-14 17:00:28 -0700

    test: fix cors issue

--- functions/getComments.js
+++ functions/getComments.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 
 module.exports = functions.https.onRequest((request, response) => {
+  response.set("Access-Control-Allow-Origin", "*");
+
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
