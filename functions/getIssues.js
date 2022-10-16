e31b7861a49058024f5e75ca4a566d366a95d88e
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2022-10-15 23:40:20 -0700

    chore: enable infinite pagination

--- functions/getIssues.js
+++ functions/getIssues.js
@@ -4,16 +4,24 @@ const {Octokit} = require("octokit");
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const octokit = new Octokit({auth: GITHUB_TOKEN});
 
-const getIssues = async (userId) => {
-  const response = await octokit.rest.search.issuesAndPullRequests({
-    q: `is:issue author:${userId}`,
-    per_page: 100,
-  });
-  return response.data.items;
+const getAllIssues = async (userId) => {
+  let hasMoreData = true;
+  const issues = [];
+  while (hasMoreData) {
+    const response = await octokit.rest.search.issuesAndPullRequests({
+      q: `is:issue author:${userId}`,
+      per_page: 100,
+    });
+    issues.push(...response.data.items);
+    if (response.data.items.length < 100) {
+      hasMoreData = false;
+    }
+  }
+  return issues;
 };
 
 module.exports = functions.https.onCall(async (userId) => {
-  const issues = await getIssues(userId);
+  const issues = await getAllIssues(userId);
 
   functions.logger.info("Hello logs!", {structuredData: true});
   return issues;
