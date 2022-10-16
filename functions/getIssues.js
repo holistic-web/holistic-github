a6b95401f866813f140656f78121650907ac2418
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2022-10-16 00:50:50 -0700

    fix: pagination

--- functions/getIssues.js
+++ functions/getIssues.js
@@ -6,13 +6,16 @@ const octokit = new Octokit({auth: GITHUB_TOKEN});
 
 const getAllIssues = async (userId) => {
   let hasMoreData = true;
+  let page = 1;
   const issues = [];
   while (hasMoreData) {
     const response = await octokit.rest.search.issuesAndPullRequests({
       q: `is:issue author:${userId}`,
       per_page: 100,
+      page,
     });
     issues.push(...response.data.items);
+    page++;
     if (response.data.items.length < 100) {
       hasMoreData = false;
     }
