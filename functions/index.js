f3cdabcc5f29f55e3e75bfc3e2217dc643e03aa1
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2022-10-18 01:29:59 -0700

    chore: refactor cloud functions into seperate functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,23 @@
-const getUserStats = require("./getUserStats");
+const functions = require("firebase-functions");
+const GithubClient = require("./GithubClient");
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.getUserStats = getUserStats;
+exports.getAllIssues = functions.https.onCall(async (userId) => {
+  const ghClient = new GithubClient();
+  const issues = await ghClient.getAllIssues(userId);
+  return issues;
+});
+
+exports.getAllPullRequests = functions.https.onCall(async (userId) => {
+  const ghClient = new GithubClient();
+  const pullRequests = await ghClient.getAllPullRequests(userId);
+  return pullRequests;
+});
+
+exports.getUser = functions.https.onCall(async (userId) => {
+  const ghClient = new GithubClient();
+  const user = await ghClient.getUser(userId);
+  return user;
+});
