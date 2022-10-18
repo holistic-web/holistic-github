d503506b306893189898f72481db00379f21c171
Michael Fitzhavey <michael.fitzhaavey@gmail.com>
2022-10-17 18:47:52 -0700

    feat: display all stale issues and pull requests as lists

--- web-app/components/ProfileTips.vue
+++ web-app/components/ProfileTips.vue
@@ -1,12 +1,32 @@
 <template>
   <section>
     <p>Items are marked stale if they haven't received activity in over a month:</p>
-    <p>
-      You have {{ stalePullRequests.length }} stale pull requests that need attention.
-    </p>
-    <p>
-      You have {{ staleIssues.length }} stale issues that need attention.
-    </p>
+
+    <section v-if="stalePullRequests.length>0">
+      <p>
+        You have {{ stalePullRequests.length }} stale pull requests that need attention.
+        <b-btn variant="primary" size="sm" @click="showPullRequests = !showPullRequests">
+          View
+        </b-btn>
+      </p>
+      <b-collapse v-model="showPullRequests">
+        {{ stalePullRequests }}
+      </b-collapse>
+    </section>
+
+    <section v-if="staleIssues.length>0">
+      <p>
+        You have {{ staleIssues.length }} stale issues that need attention.
+        <b-btn variant="primary" size="sm" @click="showIssues = !showIssues">
+          View
+        </b-btn>
+      </p>
+      <b-collapse v-model="showIssues">
+        <li v-for="issue in staleIssues" :key="issue.id">
+          <a :href="issue.html_url" target="_blank">{{ issue.title }}</a>
+        </li>
+      </b-collapse>
+    </section>
   </section>
 </template>
 
@@ -22,6 +42,12 @@ export default {
     pullRequests: {
       type: Array,
       required: true
+    }
+  },
+  data () {
+    return {
+      showPullRequests: false,
+      showIssues: false
     }
   },
   computed: {
